a5a379ef859b39a8009131fc1fe49f7d049862a9
Eska <eskapdk@gmail.com>
2023-06-12 17:29:44 +0700

    penambahan method show, add dan delete

--- src/AplikasiTodolist.java
+++ src/AplikasiTodolist.java
@@ -2,7 +2,7 @@ public class AplikasiTodolist {
     
     public static String[] data = new String[10];
     public static void main(String[] args) {
-        
+        testHapusTodoList();
     }
 
     /**
@@ -19,20 +19,94 @@ public class AplikasiTodolist {
         }
     }
 
+    public static void testTampilTodoList() {
+        data[0]="Makan";
+        data[1]="Mandi";
+        data[2]="Belajar";
+        tampilTodoList();
+    }
+
+
     /**
      * menambahkan todo list
      */
 
-    public static void tambahTodoList() {
-        
+    public static void tambahTodoList(String todo) {
+
+        //cek apakah data penuh?
+        var isFull = true;
+        for(int i=0;i<data.length;i++){
+            if(data[i]==null){
+                isFull = false;
+                break;
+            }
+        }
+
+        //jika penuh ukuran array dirubah menjadi 2x lipat
+        if(isFull){
+            var temp = data;
+            data = new String[data.length*2];
+
+            for(int i=0;i<temp.length;i++){
+                data[i]=temp[i];
+            }
+        }
+
+        // tambahkan posis data ke array yang null
+        for (int i=0;i<data.length;i++){
+            if(data[i]==null){
+                data[i]=todo;
+                break;
+            }
+        }
+    }
+
+    public static void testTambahTodoList() {
+        for (int i=0;i<25;i++) {
+            tambahTodoList("Contoh todo list ke-"+i);
+        }
+        tampilTodoList();
     }
 
     /**
      * menghapus TodoList
      */
 
-    public static void hapusTodoList() {
-        
+    public static boolean hapusTodoList(Integer number) {
+        if((number-1) >= data.length){
+            return false;
+        } else if (data[number-1]==null){
+            return false;
+        } else {
+            for (int i = (number -1);i < data.length;i++){
+                if(i == (data.length-1)){
+                    data[i] = null;
+                } else{
+                    data[i]=data[i+1];
+                }
+            }
+            return true;
+        }
+    }
+
+    public static void testHapusTodoList(){
+        tambahTodoList("satu");
+        tambahTodoList("dua");
+        tambahTodoList("tiga");
+        tambahTodoList("empat");
+        tambahTodoList("lima");
+
+        var hasil = hapusTodoList(20);
+        System.out.println(hasil);
+
+        hasil = hapusTodoList(7);
+        System.out.println(hasil);
+
+        hasil = hapusTodoList(2);
+        System.out.println(hasil);
+
+        tampilTodoList();
+
     }
 
     /**
